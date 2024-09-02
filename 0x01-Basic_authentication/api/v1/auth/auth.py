15f0938affe21af737e2cff39cc8ec3b6182012a
dexterski007 <mbouj007@gmail.com>
2024-09-02 18:52:57 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,14 @@ class Auth:
     """ Class for Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ public method for require auth """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for element in excluded_paths:
+            if path.rstrip('/') in element.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ public method for auth header """
