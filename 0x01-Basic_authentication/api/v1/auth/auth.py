f44a3f7a56ebd11afb6f4c8f49cd7e524ecb1038
dexterski007 <mbouj007@gmail.com>
2024-09-02 22:30:55 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ public method for auth header """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current User public method """
