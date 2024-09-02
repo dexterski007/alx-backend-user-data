3e5077191ba53a01f4c31d5bdaccc45b83162408
dexterski007 <mbouj007@gmail.com>
2024-09-02 18:44:52 +0100

    fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ public method for require auth """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ public method for auth header """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current User public method """
         return None
