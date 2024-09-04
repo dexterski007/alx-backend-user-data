81a4b68fc18ad0c3ad46c91bdf2cac2902737098
dexterski007 <mbouj007@gmail.com>
2024-09-04 23:04:13 +0100

    task6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 session auth module for the API
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 from uuid import uuid4
 
 
@@ -27,3 +28,9 @@ class SessionAuth(Auth):
         if not type(session_id) is str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ current user method """
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        return User.get(user_id)
