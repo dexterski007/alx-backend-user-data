47913440a83b06698d824a26d3a0fc118b7da6a8
dexterski007 <mbouj007@gmail.com>
2024-09-05 09:43:07 +0100

    pycode

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -30,8 +30,9 @@ def auth_session() -> str:
     out.set_cookie(sess, session_id)
     return out
 
+
 @app_views.route('/auth_session/logout', methods=['DELETE'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def logout_session() -> Tuple[str, int]:
     """ method for logout session """
     from api.v1.app import auth
