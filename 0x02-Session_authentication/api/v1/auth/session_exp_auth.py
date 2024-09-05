489cb6221126c17f65844d38dbbfa8fe2bada744
dexterski007 <mbouj007@gmail.com>
2024-09-05 12:00:21 +0100

    parenthesis

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -4,7 +4,7 @@ session auth expiration module for the API
 """
 from api.v1.auth.session_auth import SessionAuth
 import os
-import datetime
+from datetime import datetime, timedelta
 from models.user import User
 from uuid import uuid4
 
@@ -15,7 +15,7 @@ class SessionExpAuth(SessionAuth):
         """ constructor method for session exp"""
         super().__init__()
         try:
-            self.session_duration = int(os.getenv("SESSION_DURATION"), '0')
+            self.session_duration = int(os.getenv("SESSION_DURATION", '0'))
         except Exception:
             self.session_duration = 0
 
@@ -24,25 +24,24 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        session_dict = {}
-        session_dict["user_id"] = user_id
-        session_dict["created_at"] = datetime.datetime.now()
-        self.user_id_by_session_id[session_id] = session_dict
+        self.user_id_by_session_id[session_id] = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """ user_id for session id method """
         if session_id is None:
             return None
-        if session_id not in self.user_id_by_session_id:
-            return None
-        if self.session_duration <= 0:
+        if session_id in self.user_id_by_session_id:
             session_dict = self.user_id_by_session_id[session_id]
+            if self.session_duration <= 0:
+                return session_dict["user_id"]
+            if "created_at" not in session_dict:
+                return None
+            current_time = datetime.now()
+            elapsed_time = timedelta(seconds=self.session_duration)
+            if session_dict["created_at"] + elapsed_time < current_time:
+                return None
             return session_dict["user_id"]
-        if "created_at" not in session_dict:
-            return None
-        current_time = datetime.now()
-        elapsed_time = datetime.timedelta(seconds=self.session_duration)
-        if session_dict["created_at"] + elapsed_time < current_time:
-            return None
-        return session_dict["user_id"]
