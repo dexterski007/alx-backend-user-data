497a6f00bedbcdced227e865f0c9e16defe319d4
dexterski007 <mbouj007@gmail.com>
2024-09-05 13:36:58 +0100

    fix?

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 from models.user import User
 from uuid import uuid4
 
+format = "%Y-%m-%dT%H:%M:%S"
+
 
 class SessionDBAuth(SessionExpAuth):
     """ Class for session Auth db """
@@ -19,7 +21,8 @@ class SessionDBAuth(SessionExpAuth):
             return None
         kwargs = {
             "user_id": user_id,
-            "session_id": session_id
+            "session_id": session_id,
+            "created_at": datetime.now().strftime(format)
         }
         session = UserSession(**kwargs)
         session.save()
