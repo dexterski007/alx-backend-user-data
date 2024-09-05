1a3c723a639e5bbf9b1bb27735de69a61e422ed4
dexterski007 <mbouj007@gmail.com>
2024-09-05 13:18:24 +0100

    fixed

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -15,31 +15,38 @@ class SessionDBAuth(SessionExpAuth):
     def create_session(self, user_id=None):
         """ create session method """
         session_id = super().create_session(user_id)
-        if session_id and type(session_id) is str:
-            kwargs = {
-                "user_id": user_id,
-                "session_id": session_id
-            }
-            session = UserSession(**kwargs)
-            session.save()
-            return session_id
+        if session_id is None:
+            return None
+        kwargs = {
+            "user_id": user_id,
+            "session_id": session_id
+        }
+        session = UserSession(**kwargs)
+        session.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """ user id for session db """
-        session = UserSession.search({"session_id": session_id})
-        if session is None:
+        if session_id is None:
             return None
-        current_time = datetime.now()
-        elapsed_time = timedelta(seconds=self.session_duration)
-        if session[0].created_at + elapsed_time < current_time:
+        if super().user_id_for_session_id(session_id) is None:
+            return None
+        try:
+            session = UserSession.search({"session_id": session_id})
+        except Exception:
+            return None
+        if session is None or len(session) <= 0:
             return None
         return session[0].user_id
 
     def destroy_session(self, request=None):
         """ destroy session method """
         session_id = self.session_cookie(request)
-        session = UserSession.search({"session_id": session_id})
-        if session is None:
+        try:
+            session = UserSession.search({"session_id": session_id})
+        except Exception:
+            return False
+        if len(session) <= 0:
             return False
         session[0].remove()
         return True
