3247f56e2e03f8a441ecd137018cdbec273df274
dexterski007 <mbouj007@gmail.com>
2024-09-10 23:48:30 +0100

    task 8

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 """flask app basic
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
-def welcome() -> str:
+def welcome() -> Response:
     """ main root method"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> Response:
+    """ route for creating users """
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
