aacc7eb81fcdea1f8b3227de837902cfbd9995ab
dexterski007 <mbouj007@gmail.com>
2024-09-10 23:07:46 +0100

    fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ find user method """
         try:
             new_user = self._session.query(User).filter_by(**kwargs).one()
