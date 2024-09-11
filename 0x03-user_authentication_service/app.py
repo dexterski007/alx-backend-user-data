d3c3e089bdecf5f99bbbe5bcf4de92227f677e31
dexterski007 <mbouj007@gmail.com>
2024-09-11 21:07:40 +0100

    task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,15 @@ def profile() -> Response:
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> Response:
+    """ reset password route """
+    email = request.form['email']
+    token = AUTH.get_reset_password_token(email)
+    if token:
+        return jsonify({"email": email, "reset_token": token})
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
