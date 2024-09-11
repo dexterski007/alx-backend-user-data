c0999292311e38995b8d68607cacc6ff4e0a7535
dexterski007 <mbouj007@gmail.com>
2024-09-11 22:12:41 +0100

    fix?

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -85,7 +85,8 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
     except (ValueError, NoResultFound):
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"}), 200
+    return make_response(
+        jsonify({"email": email, "message": "Password updated"}), 200)
 
 
 if __name__ == "__main__":
