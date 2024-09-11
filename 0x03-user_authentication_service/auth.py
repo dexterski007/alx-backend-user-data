ae7617cf2bbc7182b076fbe2163965854074a3e3
dexterski007 <mbouj007@gmail.com>
2024-09-11 09:24:36 +0100

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,3 +54,13 @@ class Auth:
             return new_uuid
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ get user from session id """
+        if session_id:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                return user
+            except NoResultFound:
+                return None
+        return None
