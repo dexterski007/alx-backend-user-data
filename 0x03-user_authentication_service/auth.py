569818a874c97b8bdb296132279b284859b19b5f
dexterski007 <mbouj007@gmail.com>
2024-09-11 09:29:41 +0100

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,5 +67,4 @@ class Auth:
 
     def destroy_session(self, user_id: int):
         """ destroy session method """
-        user = self._db.find_user_by(user_id=user_id)
-        self._db.update_user(user, session_id=None)
+        self._db.update_user(user_id, session_id=None)
